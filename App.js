f98087bc61d9c6d551b7a9f08c78b4b8ebfd38ff
moura1001 <gmouraneto01@gmail.com>
2021-10-10 10:55:58 -0300

    commit com renderização da lista de tarefas

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'learn clojure', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the snes', key: '3' }
+  ]);
+
   return (
     <View style={styles.container}>
       {/* header */}
       <View style={styles.content}>
         {/* user input */}
         <View style={styles.list}>
-
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -20,4 +31,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
